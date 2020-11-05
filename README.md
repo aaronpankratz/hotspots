6e86ad221c217a6027c6d3c9a13decd551c3bf8a
Aaron Pankratz <pankratz.aaron@gmail.com>
2020-11-04 22:34:49 -0600

    added hotspots script and updated README

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # hotspots
-detect hotspots of technical debt in source code
+simple python script for detecting hotspots of technical debt in source code
+
+## requirements
+Python3 `brew install python3`
+cloc `brew install cloc`
+git `brew install git`
+
+## usage
+copy this script into your source code repository
+run the script with `python3 hotspts.py`
+the script will create a sqlite db `hotspots.db`
+connect to the db to analyze your files
+`sqlite3 hotspots.db`
+
+`select name, change_frequency, code from files order by change_frequency desc, code desc;`
+
+look for large files that have a high change frequency and consider refactoring these files.
---
+++ hotspots.py
@@ -0,0 +1,41 @@
+import os
+import csv
+import sqlite3
+
+cloc = "cloc ./ --by-file --csv --quiet --report-file=cloc.csv"
+cf = "git log --format=format: --name-only | egrep -v '^$' | sort | uniq -c | sort -r | sed -e 's/ *\([[:digit:]]*\) *\([^ ]*\)/.\/\\2,\\1/' > cf.csv"
+
+if not os.path.exists("cloc.csv"):
+    os.system(cloc)
+
+if not os.path.exists("cf.csv"):
+    os.system(cf)
+
+conn = sqlite3.connect('hotspots.db')
+c = conn.cursor()
+
+c.execute("DROP TABLE IF EXISTS files")
+c.execute('''
+    CREATE TABLE files
+    (name text, change_frequency int, blank int, comment int, code int)
+''')
+
+with open("cf.csv") as cf_file:
+    cf_reader = csv.reader(cf_file, delimiter=",")
+    for row in cf_reader:
+        insert_sql = "INSERT INTO files VALUES ('{}', {}, null, null, null)".format(row[0], row[1])
+        c.execute(insert_sql)
+
+with open("cloc.csv") as cloc_file:
+    cloc_reader = csv.reader(cloc_file, delimiter=",")
+    for row in cloc_reader:
+        update_sql = "UPDATE files SET blank={}, comment={}, code={} WHERE name='{}'".format(row[2], row[3], row[4], row[1])
+        c.execute(update_sql)
+
+conn.commit()
+conn.close()
+
+os.remove('cloc.csv')
+os.remove('cf.csv')
+
+
